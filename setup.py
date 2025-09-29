0383c0e6c4cd99dd1f6ff2b60e10d67e01d09f52
varungoyal2006 <varungoyal2006@outlook.com>
2025-09-29 22:31:15 +0530

    Data Transformation done

--- setup.py
+++ setup.py
@@ -9,18 +9,14 @@ def getrequirements(file_path:str) -> List[str]:
   requirements = []
   with open(file_path) as file_obj:
     requirements = file_obj.readlines()
-    requirementes = [req.replace("\n", "")for req in requirements]
-
-    if HYPEN_E_DOT in requirements:
-      requirements.remove(HYPEN_E_DOT)
-  
+    requirements = [req.strip() for req in requirements if req.strip() != HYPEN_E_DOT]
   return requirements
 
 setup(
   name = 'mlproject',
   version = '0.0.1',
   author = 'Varun Goyal', 
-  author_gmail = 'varungoyal2006@outlook.com',
+  author_email = 'varungoyal2006@outlook.com',
   packages = find_packages(), 
   install_requires = getrequirements('requirement.txt')
 )
